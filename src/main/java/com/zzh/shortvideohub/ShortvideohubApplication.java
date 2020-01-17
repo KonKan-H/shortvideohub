a26499e613898a32cfd742fdb3599bca33687db7
zhouzhenhua <zhouzhenhua@meorient.com>
2020-01-17 17:08:58 +0800

    first update

---
+++ src/main/java/com/zzh/shortvideohub/ShortvideohubApplication.java
@@ -0,0 +1,13 @@
+package com.zzh.shortvideohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortvideohubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortvideohubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzh/shortvideohub/ShortvideohubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzh.shortvideohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortvideohubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
