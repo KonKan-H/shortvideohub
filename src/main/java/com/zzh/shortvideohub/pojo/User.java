364574688e7bd022ef0d106e98f27cca19618b57
zhouzhenhua <zhouzhenhua@meorient.com>
2020-02-26 19:45:55 +0800

    first update

---
+++ src/main/java/com/zzh/shortvideohub/pojo/User.java
@@ -0,0 +1,13 @@
+package com.zzh.shortvideohub.pojo;
+
+import lombok.Data;
+
+/**
+ * @author zzh
+ * @version 1.0
+ * @date 2020/2/26 19:45
+ */
+@Data
+public class User {
+    private int id;
+}
